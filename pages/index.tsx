fbbd959a4e13a949a5ef24c18102271997ffc5c9
calixto <calixto@silverflow.com>
2023-06-18 12:24:30 +0200

    calculations done, lacks year by year estimation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,15 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
 import Grid from '@mui/material/Unstable_Grid2';
 import {styled} from "@mui/material/styles";
-import {
-    FormControl,
-    FormHelperText,
-    Input,
-    InputAdornment,
-    InputLabel,
-    OutlinedInput,
-    Paper,
-    TextField
-} from "@mui/material"; // Grid version 2
+import {InputAdornment, Paper, TextField} from "@mui/material"; // Grid version 2
 import {TaxBracket} from "../src/models/Models";
 import Button from "@mui/material/Button";
-import {useState} from "react";
-import { NumericFormatCustom } from '../src/helpers/NumericFormat';
-const Item = styled(Paper)(({ theme }) => ({
+import {NumericFormatCustom} from '../src/helpers/NumericFormat';
+
+const Item = styled(Paper)(({theme}) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
@@ -30,8 +18,8 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const wozYearValues: Record<string, number> = {
-    "2023": 0.45,
-    "2022": 0.35,
+    "2023": 0.0045,
+    "2022": 0.0035,
 }
 
 const deductibleInterest: Record<TaxBracket, number> = {
@@ -39,88 +27,133 @@ const deductibleInterest: Record<TaxBracket, number> = {
     "2": 0.4
 }
 
-const calculateDeductiblePossibleAmount = (wozValue: number, mortgageInterest: number, year: string): number => {
-    return mortgageInterest - (wozValue * wozYearValues[year]);
+const calculateDeductiblePossibleAmount = (wozValue: number, mortgageAmount: number): number => {
+    return mortgageAmount - (wozValue * 0.0035);
 }
 
-const calculateDeductibleInterest = (deductibleAmount: number, taxBracket: TaxBracket, year: string): number => {
-   if (year === "2022") {
-       return deductibleAmount * deductibleInterest[taxBracket];
-   }
-   return deductibleAmount * 0.37;
+const calculateDeductibleInterest = (deductibleAmount: number): number => {
+    // if (year === "2022") {
+    //     return deductibleAmount * deductibleInterest[taxBracket];
+    // }
+    return deductibleAmount * 0.37;
 }
+
+
+function calculateMortgageInterestYear(mortgageAmount: number, mortgageInterestRate: number) {
+    return mortgageAmount * mortgageInterestRate / 100;
+}
+
 export default function Home() {
     const [anualSalary, setAnualSalary] = useState(0);
     const [wozValue, setWozValue] = useState(0);
-    const [anualIncome, setAnualIncome] = useState(0);
+    const [mortgageAmount, setMortgageAmount] = useState(0);
+    const [deductibleInterest, setDeductibleInterest] = useState(0);
+    const [mortgageInterestRate, setMortgageInterestRate] = useState(0);
+    const [deductibleInterestPerYear, setDeductibleInterestPerYear] = useState(0);
+
+
+    useEffect(() => {
+        console.log(deductibleInterestPerYear)
+    }, [deductibleInterestPerYear]);
+
+
+    function calculateDeductibleInterestFromEvent(event: any) {
+        let mortageInterestYearly = calculateMortgageInterestYear(mortgageAmount, mortgageInterestRate);
+        console.log(mortageInterestYearly)
+        const calculatedDeductiblePossibleAmount = calculateDeductiblePossibleAmount(wozValue, mortageInterestYearly);
+        console.log(`Calculated deductible possible value: ${calculatedDeductiblePossibleAmount}`)
+        const calculatedDeductibleInterestYear = calculateDeductibleInterest(calculatedDeductiblePossibleAmount);
+        console.log("Deductable amount "+calculatedDeductibleInterestYear)
+        setDeductibleInterestPerYear(calculatedDeductibleInterestYear);
+    }
+
+
     return (
-    <Container maxWidth="lg">
-      {/*<Box*/}
-      {/*  sx={{*/}
-      {/*    my: 50,*/}
-      {/*      width: 1,*/}
-      {/*    display: 'flex',*/}
-      {/*    flexDirection: 'column',*/}
-      {/*    justifyContent: 'center',*/}
-      {/*    alignItems: 'center',*/}
-      {/*  }}*/}
-      {/*>*/}
-          <Grid container spacing={2}>
-              <Grid xs={12}>
-                  <Box sx={{ textAlign: 'center', m: 1 }}>Dutch mortgage interest deduction calculator</Box>
-              </Grid>
-              {/*<FormControl>*/}
-              <Grid xs={12}>
-                  <TextField
-                      id="anual-salary"
-                      label="Anual Salary"
-                      onChange={(e) => setAnualSalary(Number(e.target.value))}
-                      InputProps={{
-                          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                          inputComponent: NumericFormatCustom as any,
-                      }}
-                      fullWidth
-                  />
-              </Grid>
-              <Grid xs={4}>
-                  <TextField
-                      id="anual-income"
-                      label="Anual Income"
-                      onChange={(e) => setAnualIncome(Number(e.target.value))}
-                      InputProps={{
-                          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                          inputComponent: NumericFormatCustom as any,
-                      }}
-                      type="number"
-                  />
-              </Grid>
-              <Grid xs={4}>
-                  <TextField
-                      id="woz-value"
-                      label="WOZ Value of house"
-                      onChange={(e) => setWozValue(Number(e.target.value))}
-                      InputProps={{
-                          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                          inputComponent: NumericFormatCustom as any,
-                      }}
-                      type="number"
-                  />
-              </Grid>
-              <Grid xs={4}>
-                  <Button variant="outlined">Calculate</Button>
-              </Grid>
-              <Grid xs={4}>
-                  <Paper>
-                      WozValue: {wozValue}
-                      AnualIncome: {anualIncome}
-                      AnualSalary: {anualSalary}
-                  </Paper>
-              </Grid>
-              {/*</FormControl>*/}
-          </Grid>
-      {/*</Box>*/}
-    </Container>
-  );
+        <Container maxWidth="lg">
+            {/*<Box*/}
+            {/*  sx={{*/}
+            {/*    my: 50,*/}
+            {/*      width: 1,*/}
+            {/*    display: 'flex',*/}
+            {/*    flexDirection: 'column',*/}
+            {/*    justifyContent: 'center',*/}
+            {/*    alignItems: 'center',*/}
+            {/*  }}*/}
+            {/*>*/}
+            <Paper
+                elevation={12}
+                sx={{
+                    margin: 2,
+                    padding: 2,
+                }}
+            >
+                <Grid container spacing={3}>
+                    <Grid xs={12}>
+                        <Box sx={{textAlign: 'center', m: 1}}>Dutch mortgage interest deduction calculator</Box>
+                    </Grid>
+                    {/*<FormControl>*/}
+                    <Grid xs={6}>
+                        <TextField
+                            id="anual-salary"
+                            label="Anual Salary"
+                            onChange={(e) => setAnualSalary(Number(e.target.value))}
+                            InputProps={{
+                                inputComponent: NumericFormatCustom as any,
+                            }}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid xs={6}>
+                        <TextField
+                            id="mortgage-interest-rate"
+                            label="Mortgage Interest Rate"
+                            onChange={(e) => setMortgageInterestRate(Number(e.target.value))}
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                                inputComponent: NumericFormatCustom as any,
+                            }}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid xs={4}>
+                        <TextField
+                            id="mortage-amount"
+                            label="Mortgage Amount"
+                            onChange={(e) => setMortgageAmount(Number(e.target.value))}
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                                inputComponent: NumericFormatCustom as any,
+                            }}
+                        />
+                    </Grid>
+                    <Grid xs={4}>
+                        <TextField
+                            id="woz-value"
+                            label="WOZ Value of House"
+                            onChange={(e) => setWozValue(Number(e.target.value))}
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                                inputComponent: NumericFormatCustom as any,
+                            }}
+                        />
+                    </Grid>
+                    <Grid xs={4}>
+                        <Button variant="outlined"
+                                color="primary"
+                                onClick={calculateDeductibleInterestFromEvent}
+                        >Calculate</Button>
+                    </Grid>
+                    <Grid xs={12}>
+                        <Paper>
+                            Deductible Interest for that year: {deductibleInterestPerYear}
+                        </Paper>
+                    </Grid>
+                    {/*</FormControl>*/}
+                </Grid>
+            </Paper>
+            {/*</Box>*/}
+        </Container>
+    );
 }
 
 
